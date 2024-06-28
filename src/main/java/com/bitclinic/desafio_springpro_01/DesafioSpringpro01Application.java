5b40c887adf674e971762e4fc362f0de2d165052
amac81 <arnaldocanelas@gmail.com>
2024-06-28 12:53:53 +0100

    Class order created

--- src/main/java/com/bitclinic/desafio_springpro_01/DesafioSpringpro01Application.java
+++ src/main/java/com/bitclinic/desafio_springpro_01/DesafioSpringpro01Application.java
@@ -1,13 +1,20 @@
 package com.bitclinic.desafio_springpro_01;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioSpringpro01Application {
+public class DesafioSpringpro01Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioSpringpro01Application.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.print("OLA MUNDO !");
+		
 	}
 
 }
