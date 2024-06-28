6cb6754a50455cee9635402daaafb77a90197482
amac81 <arnaldocanelas@gmail.com>
2024-06-28 12:41:46 +0100

    Project created

---
+++ src/main/java/com/bitclinic/desafio_springpro_01/DesafioSpringpro01Application.java
@@ -0,0 +1,13 @@
+package com.bitclinic.desafio_springpro_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringpro01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSpringpro01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitclinic/desafio_springpro_01/DesafioSpringpro01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitclinic.desafio_springpro_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringpro01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
