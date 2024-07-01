4e58ed4aac264df64e64913dd2517f73383d7bb8
amac81 <arnaldocanelas@gmail.com>
2024-07-01 15:06:03 +0100

    class UI add

--- src/main/java/com/bitclinic/desafio_springpro_01/DesafioSpringpro01Application.java
+++ src/main/java/com/bitclinic/desafio_springpro_01/DesafioSpringpro01Application.java
@@ -1,8 +1,13 @@
 package com.bitclinic.desafio_springpro_01;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.bitclinic.desafio_springpro_01.utils.Ui;
 
 @SpringBootApplication
 public class DesafioSpringpro01Application implements CommandLineRunner {
@@ -12,8 +17,17 @@ public class DesafioSpringpro01Application implements CommandLineRunner {
 	}
 	@Override
 	public void run(String... args) throws Exception {
+		Locale.setDefault(Locale.US);
+		
+		//Objeto Scanner para entrada de dados, a partir de System.in 
+		Scanner sc = new Scanner(System.in);
 
-		System.out.print("OLA MUNDO !");
+		System.out.println();
+		System.out.println("Dados do pedido\n");
+		
+		int codigo = Ui.askAndValidateIntInput("Código: ", sc, 0, 99999);
+		double valorBasico = Ui.askAndValidateDoubleInput("Valor básico: ", sc);
+		double porcentagemDesconto = Ui.askAndValidateDoubleInput("Porcentagem de Desconto: ", sc);
 		
 	}
 
